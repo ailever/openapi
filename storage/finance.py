f87adaf2cfa352a91a3d3e03472beabdbf0496be
userdky <userdyk@gmail.com>
2020-10-12 16:45:25 +0900

    update

--- storage/finance.py
+++ storage/finance.py
@@ -1,17 +1,29 @@
 #%%
 import FinanceDataReader as fdr
 import matplotlib.pyplot as plt
+from pprint import pprint
 import pandas as pd
 import numpy as np
 
 #%%
-def datareader(code, period):
+markets = ['NASDAQ', 'NYSE', 'AMEX', 'SSE', 'SZSE', 'HKEX', 'TSE', 'HOSE',
+           'KRX', 'KOSPI', 'KOSDAQ', 'KONEX',
+           'KRX-DELISTING',
+           'KRX-ADMINISTRATIVE',
+           'S&P500', 'SP500']
+
+stock = fdr.StockListing('KRX').set_index('Name')
+
+#%%
+def datareader(name, period):
+    pprint(stock[stock.index == name].to_dict())
+    code = str(stock[stock.index == name].Symbol.values.squeeze())
     stock_price = fdr.DataReader(code)['Close'][-period:]
     x = stock_price.index.values
     y = stock_price.values
     return x, y
 
-x, y = datareader(code='002900', period=300)
+x, y = datareader(name='동양물산', period=52*1)
 plt.scatter(x,y, c='r', marker='*')
 plt.plot(x,y)
 plt.grid()
