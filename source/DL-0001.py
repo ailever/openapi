138362113c23c866a7abcee6655d5e2a45d8267f
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2020-12-17 11:21:13 +0900

    Create DL-0001.py

---
+++ source/DL-0001.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.linear = nn.Linear(2,2)
+
+    def forward(self, x):
+        return self.linear(x)
+
+model = Model()
+x = torch.Tensor(100,2).uniform_(0,1)
+y = model(x)
+
+summary(model, (2,))
