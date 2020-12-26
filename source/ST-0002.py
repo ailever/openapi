57edc947b15acfaf17e6a174d955ac9fe46eb7cd
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2020-12-26 16:13:19 +0900

    Update ST-0002.py

--- source/ST-0002.py
+++ source/ST-0002.py
@@ -4,20 +4,29 @@ def SARIMAEquation(trendparams:tuple=(0,0,0), seasonalparams:tuple=(0,0,0,1)):
     p, d, q = trendparams
     P, D, Q, m = seasonalparams
 
+    assert type(p) is int, 'Input parameter "p" is not an integer type.'
+    assert type(d) is int, 'Input parameter "d" is not an integer type.'
+    assert type(q) is int, 'Input parameter "q" is not an integer type.'
+    assert type(P) is int, 'Input parameter "P" is not an integer type.'
+    assert type(D) is int, 'Input parameter "D" is not an integer type.'
+    assert type(Q) is int, 'Input parameter "Q" is not an integer type.'
+    assert type(m) is int, 'Input parameter "m" is not an integer type.'
+
     Y_order = p + P*m + d + D*m
     e_order = q + Q*m
 
     # define Y, e
     Y, e = sympy.symbols('Y_t, e_t')
+    I, J = sympy.symbols('i, j')
     Y_ = {}; e_ = {}
     Y_['t'] = Y; Y__ = [ [Y_['t']] ]
     e_['t'] = e; e__ = [ [e_['t']] ]
     for i in range(1, Y_order+1):
         Y_[f't-{i}'] = sympy.symbols(f'Y_t-{i}')
-        Y__.append( [Y_[f't-{i}']] )    # Y__ = [ [Y_['t']], [Y_['t-1']], ..., [Y_['t-(p+P*m+q+Q*m)']] ]
+        Y__.append( [Y_[f't-{i}']*(I**i)] )    # Y__ = [ [Y_['t']], [Y_['t-1']], ..., [Y_['t-(p+P*m+q+Q*m)']] ]
     for i in range(1, e_order+1):   
         e_[f't-{i}'] = sympy.symbols(f'e_t-{i}')
-        e__.append( [e_[f't-{i}']] )    # e__ = [ [e_['t']], [e_['t-1']], ..., [e_['t-(q+Q*m)']] ]
+        e__.append( [e_[f't-{i}']*(J**i)] )    # e__ = [ [e_['t']], [e_['t-1']], ..., [e_['t-(q+Q*m)']] ]
 
     # define L
     L = sympy.symbols('L')
@@ -66,8 +75,40 @@ def SARIMAEquation(trendparams:tuple=(0,0,0), seasonalparams:tuple=(0,0,0,1)):
     Y_term = sympy.Matrix([Y_coeff]) * sympy.Matrix(Y__) # left-side
     e_term = sympy.Matrix([e_coeff]) * sympy.Matrix(e__) # right-side
     
-    Time_Series = Y - Y_term[0] + e_term[0]
-    Time_Series = sympy.collect(Time_Series, Y).simplify()
-    return Time_Series
+    Time_Series = {}
+    Time_Series['Y_t(i,j)'] = sympy.Poly(Y - Y_term[0] + e_term[0], (I,J))
+    Time_Series['Y_t'] = Time_Series['Y_t(i,j)'].subs(I, 1).subs(J, 1)
+    Time_Series['Coeff_of_Y'] = Time_Series['Y_t(i,j)'].subs(J, 0).all_coeffs()[::-1]
+    Time_Series['Coeff_of_e'] = Time_Series['Y_t(i,j)'].subs(I, 0).all_coeffs()[::-1]
+
+    for i in range(1, int(p+P*m+d+D*m)+1):
+        Time_Series['Y_t'] = sympy.collect(Time_Series['Y_t'], Y_[f't-{i}']).simplify()
+    for i in range(1, int(q+Q*m)+1):
+        Time_Series['Y_t'] = sympy.collect(Time_Series['Y_t'], e_[f't-{i}']).simplify()
+
+    Coeff_Y = []
+    Coeff_e = []
+    print('[** Y params]')
+    for i, coeff_Y in enumerate(Time_Series['Coeff_of_Y']):
+        if i == 0:
+            pass
+        elif i != 0:                
+            coeff_Y = coeff_Y.subs(Y_[f"t-{i}"], 1)
+            print(f't-{i} : {coeff_Y}')
+            Coeff_Y.append(coeff_Y)
+
+    print('\n[** e params]')
+    for i, coeff_e in enumerate(Time_Series['Coeff_of_e']):
+        if i == 0:
+            coeff_e = coeff_e.subs(e_[f"t"], 1)
+            print(f't : {coeff_e}')
+            Coeff_e.append(coeff_e)
+
+        elif i != 0:                
+            coeff_e = coeff_e.subs(e_[f"t-{i}"], 1)
+            print(f't-{i} : {coeff_e}')
+            Coeff_e.append(coeff_e)
+
+    return Time_Series['Y_t']
 
 SARIMAEquation((1,1,2), (2,0,1,4))
