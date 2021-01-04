020aed59535b26ffa01c34ae28be5aa7e8867a8f
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-01-04 14:37:36 +0900

    Update MA-0000.py

--- source/MA-0000.py
+++ source/MA-0000.py
@@ -66,12 +66,17 @@ O['T,0,0'] = dcc.Graph(figure=fig)
 O['T,0,1'] = dcc.Markdown(description)
 O['T,1,0'] = None
 O['T,1,1'] = None
+C = {} # color code : primary, secondary, info, success, warning, danger, light, dark
+C['T,0,0'] = [dbc.Card([dbc.CardHeader('Linkedin List'), dbc.CardBody(O['T,0,0'])], color='light', inverse=False, outline=True)]
+C['T,0,1'] = [dbc.Card([dbc.CardHeader('T,0,1'), dbc.CardBody(O['T,0,1'])], color='light', inverse=False, outline=True)]
+C['T,1,0'] = [dbc.Card([dbc.CardHeader('T,1,0'), dbc.CardBody(O['T,1,0'])], color='light', inverse=False, outline=True)]
+C['T,1,1'] = [dbc.Card([dbc.CardHeader('T,1,1'), dbc.CardBody(O['T,1,1'])], color='light', inverse=False, outline=True)]
 ################################## DASHBOARD ##################################
 contents = {}; contents['page'] = {}; page_layouts = {}
-contents['page']['tab'] = [dbc.Row([dbc.Col(O['T,0,0'], width=6), dbc.Col(O['T,0,1'], width=6)]),
-                           dbc.Row([dbc.Col(O['T,1,0'], width=6), dbc.Col(O['T,1,1'], width=6)]),
+contents['page']['tab'] = [dbc.Row([dbc.Col(C['T,0,0'], width=6), dbc.Col(C['T,0,1'], width=6)]), html.Br(),
+                           dbc.Row([dbc.Col(C['T,1,0'], width=6), dbc.Col(C['T,1,1'], width=6)]), html.Br(),
                            html.Br()]
-page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab'])), label="AILEVER1", disabled=False)])
+page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab'])), label="PAGE1", disabled=False)])
 main = html.Div([html.H2(html.A('PROJECT TITLE', href="/")),
                  html.H6('Promulgate values for a better tomorrow'),
                  html.Div([dbc.Button("Ailever", color="secondary", href='https://github.com/ailever/ailever/wiki'),
