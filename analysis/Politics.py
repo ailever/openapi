8a21ec5f6fd009581204abbd8ee9e2b54222a4ab
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-01-19 13:46:29 +0900

    Update Politics.py

--- analysis/Politics.py
+++ analysis/Politics.py
@@ -36,25 +36,50 @@ app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.the
 from plotly.subplots import make_subplots
 import plotly.graph_objs as go
 
+Component = type('Component', (dict,), {})
 
-# O[T,0,0] : Korea
-KR = html.Div([dbc.Button('Naver', color='dark', href="https://news.naver.com/"),
-               dbc.Button('서울신문', color='dark', href="http://www.seoul.co.kr/"),
-               dbc.Button('조선일보', color='dark', href="https://www.chosun.com/"),
-               dbc.Button('동아일보', color='dark', href="https://www.donga.com/"),
-               dbc.Button('경향신문', color='dark', href="http://www.khan.co.kr/"),
-               dbc.Button('한국일보', color='dark', href="https://www.hankookilbo.com/"),
-               dbc.Button('중앙일보', color='dark', href="https://joongang.joins.com/"),
-               dbc.Button('한겨례', color='dark', href="http://www.hani.co.kr/"),
-               dbc.Button('국민일보', color='dark', href="http://www.kmib.co.kr/news/index.asp"),
-               dbc.Button('세계일보', color='dark', href="http://www.segye.com/"),
-               dbc.Button('문화일보', color='dark', href="http://www.munhwa.com/")])
-# O[T,0,1] : United States
-US = html.Div([html.Embed(src='https://news.naver.com/', style={'width':'100%', 'height':'100%'})])
-# O[T,1,0] : Description
-# O[T,1,1] : Description
+# O[T1,0,0] : Korea
+KR = Component()
+KR['T1,0,0/0,0'] = html.Div([dbc.Button('Naver', color='dark', href="https://news.naver.com/"),
+                             dbc.Button('서울신문', color='dark', href="https://www.seoul.co.kr/"),
+                             dbc.Button('조선일보', color='dark', href="https://www.chosun.com/"),
+                             dbc.Button('동아일보', color='dark', href="https://www.donga.com/"),
+                             dbc.Button('경향신문', color='dark', href="http://www.khan.co.kr/"),
+                             dbc.Button('한국일보', color='dark', href="https://www.hankookilbo.com/"),
+                             dbc.Button('중앙일보', color='dark', href="https://joongang.joins.com/"),
+                             dbc.Button('한겨례', color='dark', href="http://www.hani.co.kr/"),
+                             dbc.Button('국민일보', color='dark', href="http://www.kmib.co.kr/news/index.asp"),
+                             dbc.Button('세계일보', color='dark', href="http://www.segye.com/"),
+                             dbc.Button('문화일보', color='dark', href="http://www.munhwa.com/")])
+KR['T1,0,0/1,0'] = html.Embed(src='https://news.naver.com/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/2,0'] = html.Embed(src='https://www.seoul.co.kr/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/3,0'] = html.Embed(src='https://www.chosun.com/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/4,0'] = html.Embed(src='https://www.donga.com/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/5,0'] = html.Embed(src='http://www.khan.co.kr/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/6,0'] = html.Embed(src='https://www.hankookilbo.com/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/7,0'] = html.Embed(src='https://joongang.joins.com/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/8,0'] = html.Embed(src='http://www.hani.co.kr/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/9,0'] = html.Embed(src='http://www.kmib.co.kr/news/index.asp', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/10,0'] = html.Embed(src='http://www.segye.com/', style={'width':'100%', 'height':'700px'})
+KR['T1,0,0/11,0'] = html.Embed(src='http://www.munhwa.com/', style={'width':'100%', 'height':'700px'})
+KR.main = html.Div([dbc.Row(KR['T1,0,0/0,0']),
+                    dbc.Row(KR['T1,0,0/1,0']),
+                    dbc.Row(KR['T1,0,0/2,0']),
+                    dbc.Row(KR['T1,0,0/3,0']),
+                    dbc.Row(KR['T1,0,0/4,0']),
+                    dbc.Row(KR['T1,0,0/6,0']),
+                    dbc.Row(KR['T1,0,0/7,0']),
+                    dbc.Row(KR['T1,0,0/8,0']),
+                    dbc.Row(KR['T1,0,0/9,0']),
+                    dbc.Row(KR['T1,0,0/10,0']),
+                    dbc.Row(KR['T1,0,0/11,0']),
+                    ])
 
 
+
+# O[T2,0,0] : United States
+US = Component()
+US.main = html.Div([])
 
 
 
@@ -62,21 +87,23 @@ US = html.Div([html.Embed(src='https://news.naver.com/', style={'width':'100%', 
 #%%
 ################################## DASHBOARD ##################################
 T = {}
-T['T,0,0'] = 'Korea'
-T['T,1,0'] = 'United States'
+T['T1,0,0'] = 'Press'
+T['T2,0,0'] = 'Press'
 O = {}
 O['T,_,_'] = None
-O['T,0,0'] = KR
-O['T,1,0'] = US
+O['T1,0,0'] = KR.main
+O['T2,0,0'] = US.main
 C = {} # color code : primary, secondary, info, success, warning, danger, light, dark
-C['T,0,0'] = [dbc.Card([dbc.CardHeader(T['T,0,0']), dbc.CardBody(O['T,0,0'])], color='light', inverse=False, outline=True)]
-C['T,1,0'] = [dbc.Card([dbc.CardHeader(T['T,1,0']), dbc.CardBody(O['T,1,0'])], color='light', inverse=False, outline=True)]
+C['T1,0,0'] = [dbc.Card([dbc.CardHeader(T['T1,0,0']), dbc.CardBody(O['T1,0,0'])], color='light', inverse=False, outline=True)]
+C['T2,0,0'] = [dbc.Card([dbc.CardHeader(T['T2,0,0']), dbc.CardBody(O['T2,0,0'])], color='light', inverse=False, outline=True)]
 ################################## DASHBOARD ##################################
 contents = {}; contents['page'] = {}; page_layouts = {}
-contents['page']['tab'] = [dbc.Row([dbc.Col(C['T,0,0'], width=12)]), html.Br(),
-                           dbc.Row([dbc.Col(C['T,1,0'], width=12)]), html.Br(),
-                           html.Br()]
-page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab'])), label="PAGE1", disabled=False)])
+contents['page']['tab1'] = [dbc.Row([dbc.Col(C['T1,0,0'], width=12)]), html.Br(),
+                            html.Br()]
+contents['page']['tab2'] = [dbc.Row([dbc.Col(C['T2,0,0'], width=12)]), html.Br(),
+                            html.Br()]
+page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab1'])), label="Korea", disabled=False),
+                                 dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab2'])), label="United States", disabled=False),])
 main = dbc.Jumbotron([html.H2('analysis/Politics'),
                       html.H6('Ailever : Promulgate values for a better tomorrow'), html.Hr(),
                       html.Div([dbc.Button("Home", color="secondary", href='https://ailever.github.io/'),
