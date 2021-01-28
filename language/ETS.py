1fcfb2e6ba5e8b590dfaf623bc4eebfafa411a79
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-01-29 07:53:42 +0900

    Update ETS.py

--- language/ETS.py
+++ language/ETS.py
@@ -83,6 +83,8 @@ main = dbc.Jumbotron([html.H2('language/ETS'),
                                 dbc.Button("Source", color="secondary", href='https://github.com/ailever/openapi/tree/master/language'),
                                 dbc.Button("Wiki", color="secondary", href='https://github.com/ailever/ailever/wiki'),
                                 dbc.Button("Docs", color="secondary", href='https://ailever.readthedocs.io/en/latest/language/index.html'),
+                                dbc.Button("Google Trend", color="secondary", href="https://trends.google.com/trends/explore"),
+                                dbc.Button("DataLab", color="secondary", href="https://datalab.naver.com/"),
                                 dbc.Button("Rstudio", color="secondary", href=config['R-server']+':'+config['R-port']),
                                 dbc.Button("Real-Time Analysis", id='real-time', color="secondary", href=config['visdom-server']+':'+config['visdom-port'])]),
                       html.P(id='visdom-server')])
