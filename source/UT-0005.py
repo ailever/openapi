2c2e7a578d302bed823ddc7c8a602082e3dc4116
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-02-18 00:31:05 +0900

    Update UT-0005.py

--- source/UT-0005.py
+++ source/UT-0005.py
@@ -30,20 +30,6 @@ app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.the
 
 
 
-# O[T,0,0] : Figure
-fig = make_subplots(rows=1, cols=1, subplot_titles=['TITLE'])
-fig.add_trace(go.Scatter(x=[1,2,3], y=[3,2,1], mode='lines+markers'), row=1, col=1)
-# O[T,0,1] : Description
-description1 = "Description"
-# O[T,1,0] : Description
-description2 = "Description"
-# O[T,1,1] : Description
-description3 = "Description"
-
-
-
-
-
 ################################## CODEBLOCK ##################################
 #%%
 ################################## REALTIME ##################################
@@ -62,30 +48,34 @@ def real_time_analysis(click):
 ################################## REALTIME ##################################
 #%%
 ################################## DASHBOARD ##################################
+T = {}
+T['T,0,0'] = 'Title00'
+T['T,0,1'] = 'Title01'
+T['T,1,0'] = 'Title10'
+T['T,1,1'] = 'Title11'
 O = {}
 O['T,_,_'] = None
-O['T,0,0'] = dcc.Graph(figure=fig)
-O['T,0,1'] = dcc.Markdown(description1)
-O['T,1,0'] = dcc.Markdown(description2)
-O['T,1,1'] = dcc.Markdown(description3)
+O['T,0,0'] = dcc.Markdown("description00")
+O['T,0,1'] = dcc.Markdown("description01")
+O['T,1,0'] = dcc.Markdown("description10")
+O['T,1,1'] = dcc.Markdown("description11")
 C = {} # color code : primary, secondary, info, success, warning, danger, light, dark
-C['T,0,0'] = [dbc.Card([dbc.CardHeader('T,0,0'), dbc.CardBody(O['T,0,0'])], color='light', inverse=False, outline=True)]
-C['T,0,1'] = [dbc.Card([dbc.CardHeader('T,0,1'), dbc.CardBody(O['T,0,1'])], color='light', inverse=False, outline=True)]
-C['T,1,0'] = [dbc.Card([dbc.CardHeader('T,1,0'), dbc.CardBody(O['T,1,0'])], color='light', inverse=False, outline=True)]
-C['T,1,1'] = [dbc.Card([dbc.CardHeader('T,1,1'), dbc.CardBody(O['T,1,1'])], color='light', inverse=False, outline=True)]
+C['T,0,0'] = [dbc.Card([dbc.CardHeader(T['T,0,0']), dbc.CardBody(O['T,0,0'])], color='light', inverse=False, outline=True)]
+C['T,0,1'] = [dbc.Card([dbc.CardHeader(T['T,0,1']), dbc.CardBody(O['T,0,1'])], color='light', inverse=False, outline=True)]
+C['T,1,0'] = [dbc.Card([dbc.CardHeader(T['T,1,0']), dbc.CardBody(O['T,1,0'])], color='light', inverse=False, outline=True)]
+C['T,1,1'] = [dbc.Card([dbc.CardHeader(T['T,1,1']), dbc.CardBody(O['T,1,1'])], color='light', inverse=False, outline=True)]
 ################################## DASHBOARD ##################################
 contents = {}; contents['page'] = {}; page_layouts = {}
 contents['page']['tab'] = [dbc.Row([dbc.Col(C['T,0,0'], width=6), dbc.Col(C['T,0,1'], width=6)]), html.Br(),
                            dbc.Row([dbc.Col(C['T,1,0'], width=6), dbc.Col(C['T,1,1'], width=6)]), html.Br(),
                            html.Br()]
 page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab'])), label="PAGE1", disabled=False)])
-main = html.Div([html.H2(html.A('PROJECT TITLE', href="/")),
-                 html.H6('Promulgate values for a better tomorrow'),
-                 html.Div([dbc.Button("Ailever", color="secondary", href='https://github.com/ailever/ailever/wiki'),
-                           dbc.Button("Rstudio", color="secondary", href=config['R-server']+':'+config['R-port']),
-                           dbc.Button("Real-Time Analysis", id='real-time', color="secondary", href=config['visdom-server']+':'+config['visdom-port'])]),
-                 html.P(id='visdom-server'),
-                 html.Br()])
+main = dbc.Jumbotron([html.H2(html.A('PROJECT TITLE', href="/")),
+                      html.H6('Promulgate values for a better tomorrow'), html.Hr(),
+                      html.Div([dbc.Button("Ailever", color="secondary", href='https://github.com/ailever/ailever/wiki'),
+                                dbc.Button("Rstudio", color="secondary", href=config['R-server']+':'+config['R-port']),
+                                dbc.Button("Real-Time Analysis", id='real-time', color="secondary", href=config['visdom-server']+':'+config['visdom-port'])]),
+                      html.P(id='visdom-server')])
 app.layout = html.Div([main, page_layouts['page']])
 if __name__ == '__main__':
     app.run_server(host=config['dash-server'], port=config['dash-port'], debug=True) 
