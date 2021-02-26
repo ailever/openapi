0aba6672c6e059acfb773b0818fa7bdb45f15091
ailever <56889151+ailever@users.noreply.github.com>
2021-02-26 20:16:36 +0900

    Update review.py

--- analysis/review.py
+++ analysis/review.py
@@ -1,3 +1,103 @@
+#%%
+import plotly.graph_objects as go
+
+top_labels = ['C1', 'C2', 'C3', 'C4', 'C5']
+x_data = [[21, 30, 21, 16, 12],
+          [24, 31, 19, 15, 11],
+          [27, 26, 23, 11, 13],
+          [29, 24, 15, 18, 14]]
+
+y_data = ['R1',
+          'R2',
+          'R3',
+          'R4']
+
+colors = ['rgba(38, 24, 74, 0.8)', 'rgba(71, 58, 131, 0.8)',
+          'rgba(122, 120, 168, 0.8)', 'rgba(164, 163, 204, 0.85)',
+          'rgba(190, 192, 213, 1)']
+
+fig = go.Figure()
+
+for i in range(0, len(x_data[0])):
+    for xd, yd in zip(x_data, y_data):
+        fig.add_trace(go.Bar(
+            x=[xd[i]], y=[yd],
+            orientation='h',
+            marker=dict(
+                color=colors[i],
+                line=dict(color='rgb(248, 248, 249)', width=1)
+            )
+        ))
+
+fig.update_layout(
+    xaxis=dict(
+        showgrid=False,
+        showline=False,
+        showticklabels=False,
+        zeroline=False,
+        domain=[0.15, 1]
+    ),
+    yaxis=dict(
+        showgrid=False,
+        showline=False,
+        showticklabels=False,
+        zeroline=False,
+    ),
+    barmode='stack',
+    paper_bgcolor='rgb(248, 248, 255)',
+    plot_bgcolor='rgb(248, 248, 255)',
+    margin=dict(l=120, r=10, t=140, b=80),
+    showlegend=False,
+)
+
+annotations = []
+
+for yd, xd in zip(y_data, x_data):
+    # labeling the y-axis
+    annotations.append(dict(xref='paper', yref='y',
+                            x=0.14, y=yd,
+                            xanchor='right',
+                            text=str(yd),
+                            font=dict(family='Arial', size=14,
+                                      color='rgb(67, 67, 67)'),
+                            showarrow=False, align='right'))
+    # labeling the first percentage of each bar (x_axis)
+    annotations.append(dict(xref='x', yref='y',
+                            x=xd[0] / 2, y=yd,
+                            text=str(xd[0]) + '%',
+                            font=dict(family='Arial', size=14,
+                                      color='rgb(248, 248, 255)'),
+                            showarrow=False))
+    # labeling the first Likert scale (on the top)
+    if yd == y_data[-1]:
+        annotations.append(dict(xref='x', yref='paper',
+                                x=xd[0] / 2, y=1.1,
+                                text=top_labels[0],
+                                font=dict(family='Arial', size=14,
+                                          color='rgb(67, 67, 67)'),
+                                showarrow=False))
+    space = xd[0]
+    for i in range(1, len(xd)):
+            # labeling the rest of percentages for each bar (x_axis)
+            annotations.append(dict(xref='x', yref='y',
+                                    x=space + (xd[i]/2), y=yd,
+                                    text=str(xd[i]) + '%',
+                                    font=dict(family='Arial', size=14,
+                                              color='rgb(248, 248, 255)'),
+                                    showarrow=False))
+            # labeling the Likert scale
+            if yd == y_data[-1]:
+                annotations.append(dict(xref='x', yref='paper',
+                                        x=space + (xd[i]/2), y=1.1,
+                                        text=top_labels[i],
+                                        font=dict(family='Arial', size=14,
+                                                  color='rgb(67, 67, 67)'),
+                                        showarrow=False))
+            space += xd[i]
+
+fig.update_layout(annotations=annotations)
+
+
 #%% ################################## CODEBLOCK ##################################
 from plotly.subplots import make_subplots
 import plotly.express as px
@@ -7,6 +107,8 @@ import pandas as pd
 
 Figure = type('Figure', (dict,), {}) 
 figure = Figure()
+figure['T1,2,0'] = fig
+
 levels = {}
 levels[1] = ([5,5,5,5,5], ['L1-1', 'L1-2', 'L1-3', 'L1-4', 'L1-5'])
 levels[2] = ([5,5,5], ['L2-1', 'L2-2', 'L2-3'])
@@ -87,17 +189,21 @@ app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.the
 T = {}
 T['T1,0,0'] = 'Sunburst Chart'
 T['T1,1,0'] = 'Tree Map'
+T['T1,2,0'] = 'Tree Map'
 O = {}
 O['T,_,_'] = None
 O['T1,0,0'] = dcc.Graph(figure=figure['T1,0,0'])
 O['T1,1,0'] = dcc.Graph(figure=figure['T1,1,0'])
+O['T1,2,0'] = dcc.Graph(figure=figure['T1,2,0'])
 C = {} # color code : primary, secondary, info, success, warning, danger, light, dark
 C['T1,0,0'] = [dbc.Card([dbc.CardHeader(T['T1,0,0']), dbc.CardBody(O['T1,0,0'])], color='light', inverse=False, outline=True)]
 C['T1,1,0'] = [dbc.Card([dbc.CardHeader(T['T1,1,0']), dbc.CardBody(O['T1,1,0'])], color='light', inverse=False, outline=True)]
+C['T1,2,0'] = [dbc.Card([dbc.CardHeader(T['T1,2,0']), dbc.CardBody(O['T1,2,0'])], color='light', inverse=False, outline=True)]
 ################################## DASHBOARD ##################################
 contents = {}; contents['page'] = {}; page_layouts = {}
 contents['page']['tab1'] = [dbc.Row([dbc.Col(C['T1,0,0'], width=12)]), html.Br(),
                             dbc.Row([dbc.Col(C['T1,1,0'], width=12)]), html.Br(),
+                            dbc.Row([dbc.Col(C['T1,2,0'], width=12)]), html.Br(),
                             html.Br()]
 page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab1'])), label="Part to Whole Charts", disabled=False)])
 main = dbc.Jumbotron([html.H2('analysis - review'),
