d4d2e44869b08f84a9f2f5ae06368bd6bec16a4f
ailever <56889151+ailever@users.noreply.github.com>
2021-02-26 19:42:30 +0900

    Update main.py

--- detection/main.py
+++ detection/main.py
@@ -1,5 +1,4 @@
-#%%
-################################## CODEBLOCK ##################################
+#%% ################################## CODEBLOCK ##################################
 from plotly.subplots import make_subplots
 import plotly.graph_objs as go
 
@@ -14,9 +13,7 @@ description2 = "Description"
 # O[T,1,1] : Description
 description3 = "Description"
 
-################################## CODEBLOCK ##################################
-#%%
-################################## CONFIG ##################################
+#%% ################################## CONFIG ##################################
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--vs', type=str, default='127.0.0.1', help='visdom server')
@@ -47,9 +44,8 @@ config['dash-port'] = args.dp
 #vis = Visdom(server=config['visdom-server'], port=config['visdom-port'], env='main') # python -m visdom.sever [-post, --hostname]
 #vis.close(env='main')
 app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-################################## CONFIG ##################################
-#%%
-################################## DASHBOARD ##################################
+
+#%% ################################## DASHBOARD ##################################
 T = {}
 T['T,0,0'] = ''
 T['T,0,1'] = ''
@@ -87,4 +83,3 @@ main = dbc.Jumbotron([html.H2('detection'),
 app.layout = html.Div([main, page_layouts['page']])
 if __name__ == '__main__':
     app.run_server(host=config['dash-server'], port=config['dash-port'], debug=True) 
-################################## DASHBOARD ##################################
