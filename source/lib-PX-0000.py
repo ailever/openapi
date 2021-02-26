c2bd7da5ab5ae268bf20e8e50fc602e2945f5087
ailever <56889151+ailever@users.noreply.github.com>
2021-02-26 21:21:00 +0900

    Update lib-PX-0000.py

--- source/lib-PX-0000.py
+++ source/lib-PX-0000.py
@@ -1,11 +1,22 @@
+#%%
+# https://plotly.com/python-api-reference/generated/plotly.express.timeline.html
+# https://github.com/ailever/openapi/blob/master/source/lib-PX-0000.py
+
 import plotly.express as px
 import pandas as pd
 
+
+text = ['Job A',
+        'Job B',
+        'Job C',
+        'Job 4']
+
 df = pd.DataFrame([
-    dict(Task="Job A", Start='2000', Finish='2009', Resource="A"),
-    dict(Task="Job B", Start='2009', Finish='2011', Resource="A"),
-    dict(Task="Job C", Start='2009', Finish='2020', Resource="B")
+    dict(Details=text[0], Start='2000', Finish='2009', Level="L1"),
+    dict(Details=text[1], Start='2009', Finish='2011', Level="L1"),
+    dict(Details=text[2], Start='2009', Finish='2017', Level="L2"),
+    dict(Details=text[3], Start='2017', Finish='2023', Level="L2"),
 ])
 
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Resource", color="Resource")
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Level", color="Details", text=text)
 fig.show()
