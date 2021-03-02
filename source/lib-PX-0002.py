67d750d238c980e3f30a8ff967c620c287dfff00
ailever <56889151+ailever@users.noreply.github.com>
2021-03-02 18:29:56 +0900

    Create lib-PX-0002.py

---
+++ source/lib-PX-0002.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+base = np.ones((100,1))
+sectors = np.random.multinomial(1, [.4, .3, .3], size=(100,))
+df = pd.DataFrame(np.c_[base, sectors])
+df.columns = ['Base', 'Sector A', 'Sector B', 'Sector C']
+px.treemap(df, path=df.columns[1:], values='Base')
