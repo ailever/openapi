2861b9df7786c0031d044915529874caed68d3c8
ailever <56889151+ailever@users.noreply.github.com>
2021-03-04 07:45:45 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+#%%
+################################## CODEBLOCK ##################################
+from IPython import display
+from ipywidgets import interact
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+################################## CODEBLOCK ##################################
+#%%
+################################## CONFIG ##################################
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+from visdom import Visdom
+# rstudio-server start/stop/restart # /etc/rstudio/rserver.conf
+# python -m visdom.server -p 8097 --hostname 127.0.0.1
+config = {}
+config['visdom-server'] = 'http://' + '127.0.0.1'
+config['visdom-port'] = '8097'
+config['R-server'] = 'http://' + '127.0.0.1'
+config['R-port'] = '8787'
+config['dash-server'] = '127.0.0.1'
+config['dash-port'] = '8050'
+vis = Visdom(server=config['visdom-server'], port=config['visdom-port'], env='main') # python -m visdom.sever [-post, --hostname]
+vis.close(env='main')
+app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+################################## CONFIG ##################################
+################################## REALTIME ##################################
+# Real-Time Analysis
+@app.callback(
+    Output("visdom-server", "children"),
+    Input("real-time", "n_clicks"))
+def real_time_analysis(click):
+    window = vis.line(Y=torch.Tensor(1).zero_(), opts=dict(title='TITLE'))
+    white_noise = torch.Tensor(30).normal_()
+    time_series = torch.zeros_like(white_noise)
+    for t, noise in enumerate(white_noise):
+        time_series[t] = time_series[t-1] + noise
+        vis.line(X=torch.tensor([t]), Y=torch.tensor([time_series[t]]), win=window, update='append')
+    return 'Real-Time Analysis is over.'
+################################## REALTIME ##################################
+################################## DASHBOARD ##################################
+T = {}
+T['T,0,0'] = 'Title00'
+T['T,0,1'] = 'Title01'
+T['T,1,0'] = 'Title10'
+T['T,1,1'] = 'Title11'
+O = {}
+O['T,_,_'] = None
+O['T,0,0'] = dcc.Markdown("description00")
+O['T,0,1'] = dcc.Markdown("description01")
+O['T,1,0'] = dcc.Markdown("description10")
+O['T,1,1'] = dcc.Markdown("description11")
+C = {} # color code : primary, secondary, info, success, warning, danger, light, dark
+C['T,0,0'] = [dbc.Card([dbc.CardHeader(T['T,0,0']), dbc.CardBody(O['T,0,0'])], color='light', inverse=False, outline=True)]
+C['T,0,1'] = [dbc.Card([dbc.CardHeader(T['T,0,1']), dbc.CardBody(O['T,0,1'])], color='light', inverse=False, outline=True)]
+C['T,1,0'] = [dbc.Card([dbc.CardHeader(T['T,1,0']), dbc.CardBody(O['T,1,0'])], color='light', inverse=False, outline=True)]
+C['T,1,1'] = [dbc.Card([dbc.CardHeader(T['T,1,1']), dbc.CardBody(O['T,1,1'])], color='light', inverse=False, outline=True)]
+################################## DASHBOARD ##################################
+contents = {}; contents['page'] = {}; page_layouts = {}
+contents['page']['tab'] = [dbc.Row([dbc.Col(C['T,0,0'], width=6), dbc.Col(C['T,0,1'], width=6)]), html.Br(),
+                           dbc.Row([dbc.Col(C['T,1,0'], width=6), dbc.Col(C['T,1,1'], width=6)]), html.Br(),
+                           html.Br()]
+page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab'])), label="PAGE1", disabled=False)])
+main = dbc.Jumbotron([html.H2(html.A('PROJECT TITLE', href="/")),
+                      html.H6('Promulgate values for a better tomorrow'), html.Hr(),
+                      html.Div([dbc.Button("Ailever", color="secondary", href='https://github.com/ailever/ailever/wiki'),
+                                dbc.Button("Rstudio", color="secondary", href=config['R-server']+':'+config['R-port']),
+                                dbc.Button("Real-Time Analysis", id='real-time', color="secondary", href=config['visdom-server']+':'+config['visdom-port'])]),
+                      html.P(id='visdom-server')])
+app.layout = html.Div([main, page_layouts['page']])
+if __name__ == '__main__':
+    app.run_server(host=config['dash-server'], port=config['dash-port'], debug=True) 
+################################## DASHBOARD ##################################
