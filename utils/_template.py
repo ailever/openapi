6f51cfcbd5451b1635ffe3fe04a38f9dc46616d0
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-03-09 11:31:16 +0900

    Update _template.py

--- utils/_template.py
+++ utils/_template.py
@@ -1,5 +1,16 @@
-#%%
-################################## CONFIG ##################################
+#%% ################################## CODEBLOCK ##################################
+class MetaClass(type):
+    def __new__(cls, clsname, bases, namespace):
+        namespace['__str__'] = lambda self: str(self.values)
+        namespace['values'] = None
+        return type.__new__(cls, clsname, bases, namespace)
+
+Components = MetaClass('Components', (dict,), {})
+components = Components()
+components.t00 = ''
+components.t10 = ''
+
+#%% ################################## CONFIG ##################################
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--vs', type=str, default='127.0.0.1', help='visdom server')
@@ -30,42 +41,8 @@ config['dash-port'] = args.dp
 #vis = Visdom(server=config['visdom-server'], port=config['visdom-port'], env='main') # python -m visdom.sever [-post, --hostname]
 #vis.close(env='main')
 app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-################################## CONFIG ##################################
-#%%
-################################## CODEBLOCK ##################################
-from plotly.subplots import make_subplots
-import plotly.express as px
-import plotly.graph_objs as go
-import pandas as pd
 
-# O[T,0,0] : Figure
-fig1 = make_subplots(rows=1, cols=1, subplot_titles=['TITLE'])
-fig1.add_trace(go.Scatter(x=[1,2,3], y=[3,2,1], mode='lines+markers'), row=1, col=1)
-# O[T,0,1] : Description
-
-data = {}
-data["latitude"] = [37.586786]
-data["longitude"] = [126.974736]
-data["landmark"] = ['cheongwhdae']
-data["city"] = ['seoul']
-
-df = pd.DataFrame(data)
-#df.to_csv('file.csv')
-#df = pd.read_csv("https://raw.githubusercontent.com/ailever/openapi/master/analysis/file.csv")
-fig2 = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name="landmark", hover_data=["city"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-fig2.update_layout(mapbox_style="open-street-map")
-fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-# O[T,1,0] : Description
-description2 = "Description"
-# O[T,1,1] : Description
-description3 = "Description"
-
-
-################################## CODEBLOCK ##################################
-#%%
-################################## DASHBOARD ##################################
+#%% ################################## DASHBOARD ##################################
 T = {}
 T['T,0,0'] = ''
 T['T,0,1'] = ''
@@ -73,25 +50,21 @@ T['T,1,0'] = ''
 T['T,1,1'] = ''
 O = {}
 O['T,_,_'] = None
-O['T,0,0'] = dcc.Graph(figure=fig1)
-O['T,0,1'] = dcc.Graph(figure=fig2)
-O['T,1,0'] = dcc.Markdown(description2)
-O['T,1,1'] = dcc.Markdown(description3)
+O['T,0,0'] = dcc.Markdown(components.t00)
+O['T,1,0'] = dcc.Markdown(components.t10)
 C = {} # color code : primary, secondary, info, success, warning, danger, light, dark
 C['T,0,0'] = [dbc.Card([dbc.CardHeader(T['T,0,0']), dbc.CardBody(O['T,0,0'])], color='light', inverse=False, outline=True)]
-C['T,0,1'] = [dbc.Card([dbc.CardHeader(T['T,0,1']), dbc.CardBody(O['T,0,1'])], color='light', inverse=False, outline=True)]
 C['T,1,0'] = [dbc.Card([dbc.CardHeader(T['T,1,0']), dbc.CardBody(O['T,1,0'])], color='light', inverse=False, outline=True)]
-C['T,1,1'] = [dbc.Card([dbc.CardHeader(T['T,1,1']), dbc.CardBody(O['T,1,1'])], color='light', inverse=False, outline=True)]
 ################################## DASHBOARD ##################################
 contents = {}; contents['page'] = {}; page_layouts = {}
-contents['page']['tab'] = [dbc.Row([dbc.Col(C['T,0,0'], width=6), dbc.Col(C['T,0,1'], width=6)]), html.Br(),
-                           dbc.Row([dbc.Col(C['T,1,0'], width=6), dbc.Col(C['T,1,1'], width=6)]), html.Br(),
+contents['page']['tab'] = [dbc.Row([dbc.Col(C['T,0,0'], width=12)]), html.Br(),
+                           dbc.Row([dbc.Col(C['T,1,0'], width=12)]), html.Br(),
                            html.Br()]
 page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab'])), label="PAGE1", disabled=False)])
-main = dbc.Jumbotron([html.H2('utils'),
+main = dbc.Jumbotron([html.H2('utils - template'),
                       html.H6('Ailever : Promulgate values for a better tomorrow'), html.Hr(),
                       html.Div([dbc.Button("Home", color="secondary", href='https://ailever.github.io/'),
-                                dbc.Button("Source", color="secondary", href='https://github.com/ailever/openapi/tree/master/utils'),
+                                dbc.Button("Source", color="secondary", href='https://github.com/ailever/openapi/tree/master/utils/_template.py'),
                                 dbc.Button("Google Trend", color="secondary", href="https://trends.google.com/trends/explore"),
                                 dbc.Button("DataLab", color="secondary", href="https://datalab.naver.com/"),
                                 dbc.Button('Kakao Map', color='secondary', href="https://map.kakao.com/"),
@@ -103,4 +76,3 @@ main = dbc.Jumbotron([html.H2('utils'),
 app.layout = html.Div([main, page_layouts['page']])
 if __name__ == '__main__':
     app.run_server(host=config['dash-server'], port=config['dash-port'], debug=True) 
-################################## DASHBOARD ##################################
