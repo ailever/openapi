92114bff2fbdd4053de0276141dbc580780d1c74
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-03-09 11:15:02 +0900

    Update main.py

--- utils/main.py
+++ utils/main.py
@@ -1,5 +1,43 @@
-#%%
-################################## CONFIG ##################################
+#%% ################################## CODEBLOCK ##################################
+class MetaClass(type):
+    def __new__(cls, clsname, bases, namespace):
+        namespace['__str__'] = lambda self: str(self.values)
+        namespace['values'] = None
+        return type.__new__(cls, clsname, bases, namespace)
+
+Components = MetaClass('Components', (dict,), {})
+components = Components()
+
+from plotly.subplots import make_subplots
+import plotly.express as px
+import plotly.graph_objs as go
+import pandas as pd
+
+# O[T,0,0] : Figure
+fig1 = make_subplots(rows=1, cols=1, subplot_titles=['TITLE'])
+fig1.add_trace(go.Scatter(x=[1,2,3], y=[3,2,1], mode='lines+markers'), row=1, col=1)
+# O[T,0,1] : Description
+
+data = {}
+data["latitude"] = [37.586786]
+data["longitude"] = [126.974736]
+data["landmark"] = ['cheongwhdae']
+data["city"] = ['seoul']
+
+df = pd.DataFrame(data)
+#df.to_csv('file.csv')
+#df = pd.read_csv("https://raw.githubusercontent.com/ailever/openapi/master/analysis/file.csv")
+fig2 = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name="landmark", hover_data=["city"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig2.update_layout(mapbox_style="open-street-map")
+fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+# O[T,1,0] : Description
+description2 = "Description"
+# O[T,1,1] : Description
+description3 = "Description"
+
+#%% ################################## CONFIG ##################################
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--vs', type=str, default='127.0.0.1', help='visdom server')
@@ -30,42 +68,8 @@ config['dash-port'] = args.dp
 #vis = Visdom(server=config['visdom-server'], port=config['visdom-port'], env='main') # python -m visdom.sever [-post, --hostname]
 #vis.close(env='main')
 app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-################################## CONFIG ##################################
-#%%
-################################## CODEBLOCK ##################################
-from plotly.subplots import make_subplots
-import plotly.express as px
-import plotly.graph_objs as go
-import pandas as pd
 
-# O[T,0,0] : Figure
-fig1 = make_subplots(rows=1, cols=1, subplot_titles=['TITLE'])
-fig1.add_trace(go.Scatter(x=[1,2,3], y=[3,2,1], mode='lines+markers'), row=1, col=1)
-# O[T,0,1] : Description
-
-data = {}
-data["latitude"] = [37.586786]
-data["longitude"] = [126.974736]
-data["landmark"] = ['cheongwhdae']
-data["city"] = ['seoul']
-
-df = pd.DataFrame(data)
-#df.to_csv('file.csv')
-#df = pd.read_csv("https://raw.githubusercontent.com/ailever/openapi/master/analysis/file.csv")
-fig2 = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name="landmark", hover_data=["city"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-fig2.update_layout(mapbox_style="open-street-map")
-fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-# O[T,1,0] : Description
-description2 = "Description"
-# O[T,1,1] : Description
-description3 = "Description"
-
-
-################################## CODEBLOCK ##################################
-#%%
-################################## DASHBOARD ##################################
+#%% ################################## DASHBOARD ##################################
 T = {}
 T['T,0,0'] = ''
 T['T,0,1'] = ''
@@ -103,4 +107,3 @@ main = dbc.Jumbotron([html.H2('utils'),
 app.layout = html.Div([main, page_layouts['page']])
 if __name__ == '__main__':
     app.run_server(host=config['dash-server'], port=config['dash-port'], debug=True) 
-################################## DASHBOARD ##################################
