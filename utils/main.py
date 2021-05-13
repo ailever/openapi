bd37df690ef65dbe4591b0c9e8dcb260a3563aa0
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-05-13 15:10:44 +0900

    Update main.py

--- utils/main.py
+++ utils/main.py
@@ -1,109 +1,120 @@
 #%% ################################## CODEBLOCK ##################################
+from IPython import display
+from ipywidgets import interact
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#%% ################################## CONFIG ##################################
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+#from visdom import Visdom
+# service postgresql start/stop                           # /etc/postgresql/version/main/postgresql.conf
+# python -m visdom.server -p 8097 --hostname 127.0.0.1
+# rstudio-server start/stop/restart                       # /etc/rstudio/rserver.conf
+config = {}
+config['pgAdmin4-server'] = 'http://' + '127.0.0.1'
+config['pgAdmin4-port'] = '52631'
+config['visdom-server'] = 'http://' + '127.0.0.1'
+config['visdom-port'] = '8097'
+config['R-server'] = 'http://' + '127.0.0.1'
+config['R-port'] = '8787'
+config['dash-server'] = '127.0.0.1'
+config['dash-port'] = '8050'
+#vis = Visdom(server=config['visdom-server'], port=config['visdom-port'], env='main') # python -m visdom.sever [-post, --hostname]
+#vis.close(env='main')
+app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+#%% ################################## REALTIME ##################################
+@app.callback(
+    Output("visdom-server", "children"),
+    Input("real-time", "n_clicks"))
+def real_time_analysis(click):
+    return 'Real-Time Analysis is over.'
+#%% ################################## DASHBOARD ##################################
 class MetaClass(type):
     def __new__(cls, clsname, bases, namespace):
         namespace['__str__'] = lambda self: str(self.values)
         namespace['values'] = None
         return type.__new__(cls, clsname, bases, namespace)
 
-Components = MetaClass('Components', (dict,), {})
-components = Components()
+Component = MetaClass('Component', (dict,), {})
+TAB1 = Component()
+TAB1.RC00 = Component()
+TAB1.RC01 = Component()
+TAB1.RC10 = Component()
+TAB1.RC11 = Component()
+TAB1.RC20 = Component()
+TAB1.RC21 = Component()
 
-from plotly.subplots import make_subplots
-import plotly.express as px
-import plotly.graph_objs as go
-import pandas as pd
-
-# O[T,0,0] : Figure
-fig1 = make_subplots(rows=1, cols=1, subplot_titles=['TITLE'])
-fig1.add_trace(go.Scatter(x=[1,2,3], y=[3,2,1], mode='lines+markers'), row=1, col=1)
-# O[T,0,1] : Description
-
-data = {}
-data["latitude"] = [37.586786]
-data["longitude"] = [126.974736]
-data["landmark"] = ['cheongwhdae']
-data["city"] = ['seoul']
-
-df = pd.DataFrame(data)
-#df.to_csv('file.csv')
-#df = pd.read_csv("https://raw.githubusercontent.com/ailever/openapi/master/analysis/file.csv")
-fig2 = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name="landmark", hover_data=["city"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-fig2.update_layout(mapbox_style="open-street-map")
-fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-# O[T,1,0] : Description
-description2 = "Description"
-# O[T,1,1] : Description
-description3 = "Description"
-
-#%% ################################## CONFIG ##################################
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--vs', type=str, default='127.0.0.1', help='visdom server')
-parser.add_argument('--vp', type=str, default='8097', help='visdom port')
-parser.add_argument('--rs', type=str, default='127.0.0.1', help='Rstudio server')
-parser.add_argument('--rp', type=str, default='8787', help='Rstudio port')
-parser.add_argument('--ds', type=str, default='127.0.0.1', help='dash server')
-parser.add_argument('--dp', type=str, default='8050', help='dash port')
-args = parser.parse_args()
-
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
-# rstudio-server start/stop/restart # /etc/rstudio/rserver.conf
-# python -m visdom.server -p 8097 --hostname 127.0.0.1
-config = {}
-config['visdom-server'] = 'http://' + args.vs
-config['visdom-port'] = args.vp
-config['R-server'] = 'http://' + args.rs
-config['R-port'] = args.rp
-config['dash-server'] = args.ds
-config['dash-port'] = args.dp
-#import torch
-#import torch.nn as nn
-#from visdom import Visdom
-#vis = Visdom(server=config['visdom-server'], port=config['visdom-port'], env='main') # python -m visdom.sever [-post, --hostname]
-#vis.close(env='main')
-app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-#%% ################################## DASHBOARD ##################################
+TAB1.RC00.values = dcc.Markdown("""
+## Hello, Ailever!
+""")
+TAB1.RC01.values = dcc.Markdown("""
+## This is a worksheet.
+""")
+TAB1.RC10.values = html.Div([dbc.Button('A', color='dark', href=""),
+                             dbc.Button('B', color='dark', href=""),
+                             ])
+TAB1.RC11.values = html.Div([dbc.Button('A', color='dark', href=""),
+                             dbc.Button('B', color='dark', href=""),
+                             ])
+TAB1.RC20.values = html.Div([dbc.Button('A', color='dark', href=""),
+                             dbc.Button('B', color='dark', href=""),
+                             ])
+TAB1.RC21.values = html.Div([dbc.Button('A', color='dark', href=""),
+                             dbc.Button('B', color='dark', href=""),
+                             ])
+################################## DASHBOARD ##################################
 T = {}
-T['T,0,0'] = ''
-T['T,0,1'] = ''
-T['T,1,0'] = ''
-T['T,1,1'] = ''
+T['T,0,0'] = 'T__'
+T['T,0,1'] = 'T__'
+T['T,1,0'] = 'T__'
+T['T,1,1'] = 'T__'
+T['T,2,0'] = 'T__'
+T['T,2,1'] = 'T__'
 O = {}
 O['T,_,_'] = None
-O['T,0,0'] = dcc.Graph(figure=fig1)
-O['T,0,1'] = dcc.Graph(figure=fig2)
-O['T,1,0'] = dcc.Markdown(description2)
-O['T,1,1'] = dcc.Markdown(description3)
+O['T,0,0'] = TAB1.RC00.values
+O['T,0,1'] = TAB1.RC01.values
+O['T,1,0'] = TAB1.RC10.values
+O['T,1,1'] = TAB1.RC11.values
+O['T,2,0'] = TAB1.RC20.values
+O['T,2,1'] = TAB1.RC21.values
 C = {} # color code : primary, secondary, info, success, warning, danger, light, dark
 C['T,0,0'] = [dbc.Card([dbc.CardHeader(T['T,0,0']), dbc.CardBody(O['T,0,0'])], color='light', inverse=False, outline=True)]
 C['T,0,1'] = [dbc.Card([dbc.CardHeader(T['T,0,1']), dbc.CardBody(O['T,0,1'])], color='light', inverse=False, outline=True)]
 C['T,1,0'] = [dbc.Card([dbc.CardHeader(T['T,1,0']), dbc.CardBody(O['T,1,0'])], color='light', inverse=False, outline=True)]
 C['T,1,1'] = [dbc.Card([dbc.CardHeader(T['T,1,1']), dbc.CardBody(O['T,1,1'])], color='light', inverse=False, outline=True)]
+C['T,2,0'] = [dbc.Card([dbc.CardHeader(T['T,2,0']), dbc.CardBody(O['T,2,0'])], color='light', inverse=False, outline=True)]
+C['T,2,1'] = [dbc.Card([dbc.CardHeader(T['T,2,1']), dbc.CardBody(O['T,2,1'])], color='light', inverse=False, outline=True)]
 ################################## DASHBOARD ##################################
 contents = {}; contents['page'] = {}; page_layouts = {}
 contents['page']['tab'] = [dbc.Row([dbc.Col(C['T,0,0'], width=6), dbc.Col(C['T,0,1'], width=6)]), html.Br(),
                            dbc.Row([dbc.Col(C['T,1,0'], width=6), dbc.Col(C['T,1,1'], width=6)]), html.Br(),
+                           dbc.Row([dbc.Col(C['T,2,0'], width=6), dbc.Col(C['T,2,1'], width=6)]), html.Br(),
                            html.Br()]
 page_layouts['page'] = dbc.Tabs([dbc.Tab(dbc.Card(dbc.CardBody(contents['page']['tab'])), label="PAGE1", disabled=False)])
-main = dbc.Jumbotron([html.H2('utils'),
-                      html.H6('Ailever : Promulgate values for a better tomorrow'), html.Hr(),
-                      html.Div([dbc.Button("Home", color="secondary", href='https://ailever.github.io/'),
-                                dbc.Button("Source", color="secondary", href='https://github.com/ailever/openapi/tree/master/utils'),
-                                dbc.Button("Google Trend", color="secondary", href="https://trends.google.com/trends/explore"),
-                                dbc.Button("DataLab", color="secondary", href="https://datalab.naver.com/"),
-                                dbc.Button('Kakao Map', color='secondary', href="https://map.kakao.com/"),
-                                dbc.Button('Google Map', color='secondary', href="https://www.google.co.kr/maps/"),
-                                dbc.Button("Plotly", color="secondary", href="https://plotly.com/python/"),
+main = dbc.Jumbotron([html.H2(html.A('WS', href="/")),
+                      html.H6('Promulgate values for a better tomorrow'), html.Hr(),
+                      html.Div([dbc.Button("Ailever", color="secondary", href='https://ailever.github.io/'),
+                                dbc.Button("Source", color="secondary", href='https://github.com/ailever/openapi/blob/master/work-sheet/WS.py'),
+                                dbc.Button("Notion", color="secondary", href="https://www.notion.so/WorkSheet-d64a1a09956d4318ac38b3d7f0131cfb"),                                
+                                dbc.Button("pgAdmin4", color="secondary", href=config['pgAdmin4-server']+':'+config['pgAdmin4-port']),
                                 dbc.Button("Rstudio", color="secondary", href=config['R-server']+':'+config['R-port']),
                                 dbc.Button("Real-Time Analysis", id='real-time', color="secondary", href=config['visdom-server']+':'+config['visdom-port'])]),
                       html.P(id='visdom-server')])
 app.layout = html.Div([main, page_layouts['page']])
 if __name__ == '__main__':
     app.run_server(host=config['dash-server'], port=config['dash-port'], debug=True) 
+################################## SETUP INFO ##################################
+"""
+[name] : -
+[version] : 0.0
+[description] : -
+[author] : anonym
+[keywords] : -
+"""    
