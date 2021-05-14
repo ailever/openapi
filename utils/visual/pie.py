7fe5feb72cd641876c31aca759bf14e07afbdde0
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-05-14 18:56:00 +0900

    Update pie.py

--- utils/visual/pie.py
+++ utils/visual/pie.py
@@ -123,6 +123,7 @@ main = dbc.Jumbotron([html.H2(html.A('pie', href="/")),
                                 dbc.Button('plotly-ref', color='dark', href="https://plotly.com/python-api-reference/"),
                                 dbc.Button('updating figure', color='dark',  href="https://plotly.com/python/creating-and-updating-figures/"),                                
                                 dbc.Button('API(GO)', color='danger', href="https://plotly.com/python-api-reference/generated/plotly.graph_objects.Pie.html"),
+                                dbc.Button('API(PX)', color='danger', href=""),                                
                                 dbc.Button('Example', color='danger', href="https://plotly.com/python/pie-charts/")]),
                       html.P(id='visdom-server')])
 app.layout = html.Div([main, page_layouts['page']])
