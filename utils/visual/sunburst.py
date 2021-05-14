02afd799b7c17c712fdd21edbfa597f732c24590
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2021-05-14 18:55:18 +0900

    Update sunburst.py

--- utils/visual/sunburst.py
+++ utils/visual/sunburst.py
@@ -125,7 +125,7 @@ main = dbc.Jumbotron([html.H2(html.A('sunburst', href="/")),
                                 dbc.Button('plotly', color='dark', href="https://plotly.com/python/"),
                                 dbc.Button('plotly-ref', color='dark', href="https://plotly.com/python-api-reference/"),
                                 dbc.Button('updating figure', color='dark',  href="https://plotly.com/python/creating-and-updating-figures/"),                                
-                                dbc.Button('API', color='danger', href="https://plotly.com/python-api-reference/generated/plotly.graph_objects.Sunburst.html"),
+                                dbc.Button('API(GO)', color='danger', href="https://plotly.com/python-api-reference/generated/plotly.graph_objects.Sunburst.html"),
                                 dbc.Button('Example', color='danger', href="https://plotly.com/python/sunburst-charts/"),
                                 ]),
                       html.P(id='visdom-server')])
